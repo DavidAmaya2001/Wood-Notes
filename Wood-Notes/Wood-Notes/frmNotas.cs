063761d671ca9b088532c17435f8ae139d4fc3a9
David <gamerbloom5@gmail.com>
2023-01-30 15:59:41 -0600

    Agregando funciones Edit y Delete a DataGridView

--- Wood-Notes/Wood-Notes/frmNotas.cs
+++ Wood-Notes/Wood-Notes/frmNotas.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using Wood_Notes.Models;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,17 +19,20 @@ namespace Wood_Notes
     {
         public frmNotas()
         {
+            
             // Carga de Datos al form de Notas
             InitializeComponent();
             DataTable Tabla = conexion.ConsultaNotas();
             dgvContenedor.DataSource = Tabla;
+
             //dgvContenedor.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            dgvContenedor.Columns[0].Width = 10;
+            dgvContenedor.Columns[0].Width = 100;
             dgvContenedor.Columns[0].Visible = false;
             //dgvContenedor.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            dgvContenedor.Columns[1].Width = 317;
+            dgvContenedor.Columns[1].Width = 400;
             //dgvContenedor.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             dgvContenedor.Columns[2].Width = 605;
+            dgvContenedor.Columns[2].Visible = false;
             //dgvContenedor.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             dgvContenedor.Columns[3].Width = 10;
             dgvContenedor.Columns[3].Visible = false;
@@ -36,6 +40,42 @@ namespace Wood_Notes
             dgvContenedor.Columns[4].Width = 136;
             dgvContenedor.Columns[5].Visible = false;
             dgvContenedor.Columns[6].Visible = false;
+
+            
+            // Agregado de columna de edit
+            DataGridViewImageColumn columna_edit = new DataGridViewImageColumn();
+            columna_edit.Name = "Editar";
+            columna_edit.HeaderText = "Editar";
+            columna_edit.Width = 80;
+            columna_edit.DisplayIndex = 7;
+            string ruta = @"D:\Users\Documentos\GitHub TuTioElPollo\Wood-Notes\Wood-Notes\Wood-Notes\Images\editico.png";
+            byte[] bufferedit = File.ReadAllBytes(ruta);
+            using (MemoryStream ms = new MemoryStream(bufferedit))
+            {
+                columna_edit.Image = Image.FromStream(ms);
+            }
+
+            dgvContenedor.Columns.Add(columna_edit);
+            dgvContenedor.Columns["Editar"].DisplayIndex = 7;
+
+            // Agregado de columna de delete
+            DataGridViewImageColumn columna_delete = new DataGridViewImageColumn();
+            columna_delete.Name = "Eliminar";
+            columna_delete.HeaderText = "Eliminar";
+            columna_delete.Width = 80;
+            columna_delete.DisplayIndex = 8;
+            string ruta2 = @"D:\Users\Documentos\GitHub TuTioElPollo\Wood-Notes\Wood-Notes\Wood-Notes\Images\trashico.png";
+            byte[] bufferdelete = File.ReadAllBytes(ruta2);
+            using(MemoryStream ms = new MemoryStream(bufferdelete))
+            {
+                columna_delete.Image = Image.FromStream(ms);
+            }
+
+            dgvContenedor.Columns.Add(columna_delete);
+            dgvContenedor.Columns["Eliminar"].DisplayIndex = 8;
+
+            
+
 
             // Diseño por defecto del DataGridView
             DataGridViewCellStyle stylegeneral = new DataGridViewCellStyle();
@@ -146,39 +186,54 @@ namespace Wood_Notes
         {
             /* Toma de datos de la celda seleccionada del datagridview y compartiendolas al formulario secundario
                donde se realizarán los cambios */
-            if (e.RowIndex >= 0)
+            if (e.RowIndex >= 0 && dgvContenedor.Columns[e.ColumnIndex].Name=="Editar")
             {
                 // Guardado de datos en variables para almacenarlos en la clase Load
-                int idNota = Convert.ToInt32(dgvContenedor.SelectedRows[0].Cells[0].Value);
-                string Titulo = dgvContenedor.SelectedRows[0].Cells[1].Value.ToString();
-                string Contenido = dgvContenedor.SelectedRows[0].Cells[2].Value.ToString();
-                string Creacion = dgvContenedor.SelectedRows[0].Cells[3].Value.ToString();
-                string Fecha = dgvContenedor.SelectedRows[0].Cells[4].Value.ToString();
-                int Caracteres = Convert.ToInt32(dgvContenedor.SelectedRows[0].Cells[5].Value);
+                    int idNota = Convert.ToInt32(dgvContenedor.SelectedRows[0].Cells["IdNota"].Value);
+                    string Titulo = dgvContenedor.SelectedRows[0].Cells["Titulo"].Value.ToString();
+                    string Contenido = dgvContenedor.SelectedRows[0].Cells["Contenido"].Value.ToString();
+                    string Creacion = dgvContenedor.SelectedRows[0].Cells["Fecha"].Value.ToString();
+                    string Fecha = dgvContenedor.SelectedRows[0].Cells["Modificacion"].Value.ToString();
+                    int Caracteres = Convert.ToInt32(dgvContenedor.SelectedRows[0].Cells["Caracteres"].Value);
 
-                //MessageBox.Show(idNota + " " + Titulo + " " + Contenido + "" + Fecha);
 
-                // Guardado de datos en la clase Load
-                LoadData Load = new LoadData();
-                Load.setIdNota(idNota);
-                Load.setTituloNota(Titulo);
-                Load.setContenidoNota(Contenido);
-                Load.setFechaNota(Creacion);
-                Load.setFechaModificacion(Fecha);
-                Load.setCaracteres(Caracteres);
 
-                frmNotasSubMenu2 formulario = new frmNotasSubMenu2();
-                frmDetalles formulariodetalles = new frmDetalles();
-                
-                // Moviendo a segundo formulario los datos para usarse tanto para modificar, eliminar o ver detalles
-                formulario.txtId.Text = Convert.ToString(Load.getIdNota());
-                formulario.txtCreacion.Text = Load.getFechaNota();
-                formulario.txtMod.Text = Load.getFechaModificacion();
-                formulario.txtTitulo.Text = Load.getTituloNota();
-                formulario.rtxtNota.Text = Load.getContenidoNota();
-                formulario.dtpNewDate.Value = Convert.ToDateTime(Load.getFechaModificacion());
-                formulario.lblcontador.Text = Load.getCaracteres().ToString();
-                formulario.ShowDialog();
+                    //MessageBox.Show(idNota + " " + Titulo + " " + Contenido + "" + Fecha);
+
+                    // Guardado de datos en la clase Load
+                    LoadData Load = new LoadData();
+                    Load.setIdNota(idNota);
+                    Load.setTituloNota(Titulo);
+                    Load.setContenidoNota(Contenido);
+                    Load.setFechaNota(Creacion);
+                    Load.setFechaModificacion(Fecha);
+                    Load.setCaracteres(Caracteres);
+
+                    frmNotasSubMenu2 formulario = new frmNotasSubMenu2();
+                    frmDetalles formulariodetalles = new frmDetalles();
+
+                    // Moviendo a segundo formulario los datos para usarse tanto para modificar, eliminar o ver detalles
+                    formulario.txtId.Text = Convert.ToString(Load.getIdNota());
+                    formulario.txtCreacion.Text = Load.getFechaNota();
+                    formulario.txtMod.Text = Load.getFechaModificacion();
+                    formulario.txtTitulo.Text = Load.getTituloNota();
+                    formulario.rtxtNota.Text = Load.getContenidoNota();
+                    formulario.dtpNewDate.Value = Convert.ToDateTime(Load.getFechaModificacion());
+                    formulario.lblcontador.Text = Load.getCaracteres().ToString();
+                    formulario.ShowDialog();
+            }
+            else if (e.RowIndex >= 0 && dgvContenedor.Columns[e.ColumnIndex].Name == "Eliminar")
+            {
+                DialogResult resultado = MessageBox.Show("¿Está seguro que desea eliminar la siguiente nota?", "Eliminar", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+                if (resultado == DialogResult.OK)
+                {
+                    int idNota = Convert.ToInt32(dgvContenedor.SelectedRows[0].Cells["IdNota"].Value);
+                    Conexion conexion = new Conexion();
+                    conexion.AbrirConexion();
+                    conexion.EliminarDato(idNota);
+                    conexion.CerrarConexion();
+                }
             }
 
         }
