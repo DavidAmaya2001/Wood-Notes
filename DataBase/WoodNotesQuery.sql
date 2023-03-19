c84ebf4a5f442f4b5dbfeebf559bf2717684631d
David <gamerbloom5@gmail.com>
2023-03-18 19:55:45 -0600

    Nuevas modificaciones, estructuracion y encriptacion de contraseñas

--- DataBase/WoodNotesQuery.sql
+++ DataBase/WoodNotesQuery.sql
@@ -1,9 +1,11 @@
-create database WoodNotesDB
-drop database WoodNotesDB   /*Ojo a la linea*/
+create database WoodNotesDB  /* Creación de la base de datos */
 
-use WoodNotesDB
+/* Alerta */
+drop database WoodNotesDB   /* Eliminar la base de datos completa */
 
-------------------- CREACION DE TABLAS -----------------------
+use WoodNotesDB /* Uso de la base de datos */
+
+------------------------------------------------------------- CREACION DE TABLAS -------------------------------------------------------------------------------------
 
 --Relación 1:N
 -- Users 1 : N UserNotes
@@ -12,8 +14,9 @@ create table Users(
 idUsers int identity(1,1),
 nombre varchar(25) not null,
 apellido varchar(25),
-pais varchar(20) not null,
-telefono varchar(15) not null,
+pais varchar(15) not null,
+codigo varchar(6) not null,
+telefono varchar(9) not null,
 foto image,
 fecha_union date not null,
 --Llave primaria --
@@ -38,11 +41,9 @@ primary key(idNota)
 create table UserCredentials(
 idCredencial int identity(1,1),
 nickname varchar(15) not null,
-pPassword varchar(30) not null,
+pPassword varbinary(1200) not null,
 correo varchar(50) not null,
 )
-
-alter table UserNotes add idUsers int not null;
 
 -- Agregando Llave Foranea relacionando UserNotes con Users
 alter table UserNotes add constraint FK_UserNotes_Users
@@ -51,39 +52,66 @@ foreign key(idUsers) references Users(idUsers);
 alter table UserCredentials add constraint FK_Credentials_Users
 foreign key(idCredencial) references Users(idUsers);
 
----------------------------- PRUEBAS --------------------------------
+-- hashKey para las Passwords Encriptadas
+declare @hashkey varchar(25)
+set @hashkey = 'wood-notes'
+print @hashkey
 
+--------------------------------------------------------------------------- PRUEBAS ------------------------------------------------------------------------------------
+
+/* Alerta */
 drop table Users
-drop table UserCredentials
-drop table UserNotes   /*Ojo a la linea*/
+drop table UserCredentials    /* Eliminación de tablas */
+drop table UserNotes   
 
----- Tabla Users y UserCredentials----
+--------------------------- Tabla Users y UserCredentials ------------------------------------------
+
+/* Seleccion de tablas de Usuario */
+
 select * from Users
 select*from UserCredentials
 
-insert into Users values('Invitado',null,'Default','Default',null,'2023-02-26')
-insert into UserCredentials values ('Invitado','Invitado','example@gmail.com')
-insert into Users values('Juan Francisco','Perez Muñoz','El Salvador','2273-6000',null,'2023-02-23')
-insert into UserCredentials values('El pepe','12345678','elpepe@gmail.com')
+/* Creacion de usuarios [Invitado] y pruebas para la base de datos */
+declare @hashkey varchar(25)
+set @hashkey = 'wood-notes'
+insert into Users values('Invitado',null,'Default','Def','Default',null,'2023-02-26')
+insert into UserCredentials values ('Invitado',ENCRYPTBYPASSPHRASE(@hashkey,'Invitado'),'example@gmail.com')
+
+declare @hashkey varchar(25)
+set @hashkey = 'wood-notes'
+insert into Users values('Juan Francisco','Perez Muñoz','El Salvador','+(503)','2273-6000',null,'2023-02-23')
+insert into UserCredentials values('El pepe',ENCRYPTBYPASSPHRASE(@hashkey,'87654321'),'elpepe@gmail.com')
+
+declare @hashkey varchar(25)
+set @hashkey = 'wood-notes'
 insert into Users values('Pedro Melgar','Perez Bermudez','El Salvador','6163-1643',null,'2023-02-23')
-insert into UserCredentials values('Tilin','87654321','tilinsupremo@gmail.com')
+insert into UserCredentials values('Tilin',ENCRYPTBYPASSPHRASE(@hashkey,'contrasenasecreta','tilinsupremo@gmail.com')
 
+/* Eliminacion de usuarios de pruebas */
+delete from Users where idUsers = 4
+delete from UserCredentials where idCredencial = 4
 
-delete from Users where idUsers = 3
-delete from UserCredentials where idCredencial = 3
 -- Reset de Identity --
 DBCC CHECKIDENT ('Users',RESEED,1)
 DBCC CHECKIDENT ('UserCredentials',RESEED,1)
 
-----Tabla UserNotes ----
+/* Seleccion de usuarios en base a Nickname y Password encriptada */
+
+declare @hashkey varchar(25)
+set @hashkey = 'wood-notes'
+select idCredencial,nickname,pPassword from UserCredentials where nickname = 'El pepe' and CONVERT(varchar(max),DECRYPTBYPASSPHRASE(@hashkey,pPassword)) = '87654321'
+
+---------------------------------------- Tabla UserNotes ------------------------------------------
+
 select * from UserNotes
 insert into UserNotes values('Wazaaaaa','Alerta presente que camaron que se duerme se lo lleva la corriente','2023-02-23','2023-02-23',66,null,2)
 insert into UserNotes values('Nota secreta','Vamono de fiesta factory, pero lleva mucho botiquin','2023-02-23','2023-02-23',60,null,2)
 insert into UserNotes values('Nota secreta pero del otro usuario','No llego tilin y tampoco ete sech','2023-02-23','2023-02-23',45,null,3)
 
-select * from UserNotes where idUsers = 2
 
+/* Reset de Tabla */
 truncate table UserNotes
 
----- Actualizaciones ----
+----------------------------------------- Actualizaciones ------------------------------------------
+
 update UserNotes set titulo = 'El wazaaaa supremo', contenido = 'Alerta presente que camaron que se duerme se lo lleva la corriente vol.2 del batallon atlacatl', modificacion = '2023-02-24' where idNota=1 and idUsers = 1
